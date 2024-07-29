f99afcdb4333fa39fed4369667b0cb3be49f220f
jernej <jernej.hozjan12@gmail.com>
2024-07-30 00:10:40 +0200

    feat: navbar added

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,17 +1,24 @@
 import {Component} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {RouterOutlet} from '@angular/router';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
+import {RouterLink, RouterOutlet} from '@angular/router';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from "@angular/material/button";
 import {MatIconModule} from "@angular/material/icon";
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [CommonModule, RouterOutlet, MatToolbarModule, MatButtonModule, MatIconModule],
+    imports: [CommonModule, RouterOutlet, MatToolbarModule, MatButtonModule, MatIconModule, NgOptimizedImage, RouterLink],
     templateUrl: './app.component.html',
 })
 
 export class AppComponent {
     title = 'cliMB';
+
+    constructor(private router: Router) { }
+
+    goHome() {
+        this.router.navigate(["/"]);
+    }
 }
