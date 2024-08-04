2613f544528dc87a14f7079a932ec459e620d3a1
abno301 <jernej.hozjan12@gmail.com>
2024-08-04 19:13:50 +0200

    feat: Auth service added

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,24 +1,30 @@
 import {Component, OnInit} from '@angular/core';
 import { Router } from '@angular/router';
 import {KeycloakService} from "keycloak-angular";
+import {AuthService} from "./services/auth.service";
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
     title = 'cliMB';
 
-    constructor(private router: Router) { }
+    constructor(private router: Router, private keycloakService: KeycloakService, protected authService: AuthService) { }
 
-    // ngOnInit() {
-    //     if (this.keycloakService.isLoggedIn()) {
-    //         console.log('User is logged in:');
-    //     }
-    // }
+    ngOnInit() {
+        this.authService.checkLoginStatus();
+    }
 
     goHome() {
         this.router.navigate(["/"]);
+    }
+    goNakupKart() {
+        this.router.navigate(["/nakup-kart"]);
+    }
+
+    logout() {
+        this.keycloakService.logout(window.location.origin);
     }
 }
