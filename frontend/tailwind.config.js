6075127a6d16148065624347c4476d22243f5422
abno301 <jernej.hozjan@student.um.si>
2024-08-14 09:32:57 +0200

    feat: home page cards styled

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,10 @@ module.exports = {
   ],
   theme: {
     colors: {
-      primary: "#1194be",
+      primary: {
+        DEFAULT: "rgb(17, 148, 190)",
+        light: "rgb(17, 148, 190, 0.5)"
+      },
       accent: "#2e2e2e"
     },
     extend: {},
